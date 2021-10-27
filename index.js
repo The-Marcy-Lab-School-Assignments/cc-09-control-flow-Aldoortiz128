455d8cf6ac34657e85b3ec9f6a4314f39f4fd688
aldoortiz128 <aldoortiz128@gmail.com>
2021-10-27 14:23:35 +0000

    challenges finished

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
 //1
-function betweenFiveAndTen(){
+function betweenFiveAndTen(int){
   
+   
+if (int <= 10 && int >= 5) {
+return true; }
+else {return false;}
 }
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(int){
+    
+if (int <=30 && int >= 10){
+return true; }
+else {return false;}
 }
 
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(int){
+if (int <=50 && int >=30) {
+return true; }
+else {return false;}
 }
+
 
 
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(int){
+if (int <=0 && int>=30 || int <=70 && int>=50) {
+return true; }
+else {return false;}
 }
+
 
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(int){
+if (int <=50 && int>=0 || int <=60 && int>=50) {
+return true; }
+else {return false;}
 }
 
+
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(int){
+if (int <=20 && int>=0 || int <=30 && int>=20) {
+return true; }
+else {return false;}
 }
+
+console.log(betweenFiveAndTen(20))
+console.log(betweenTenAndThirty(20))
+console.log(betweenThirtyAndFifty(20))
+console.log(betweenThirtyFiftySeventy(20))
+console.log(betweenTwentyFiftySixty(20))
+console.log(betweenTenTwentyThirty(20))
+
